29bf7ebf6b33ee05bbebb336966030b44bcd8851
cleberleao <cleberleao_14@hotmail.com>
2024-11-25 17:33:04 -0300

    update docekerfile

---
+++ script_html.sh
@@ -0,0 +1,7 @@
+cd /home/opc/projeto
+sudo rm -rf front-teste
+git clone https://github.com/cleberleao/front-teste.git
+cd /home/opc/projeto/front-teste
+sudo podman build -t minha-pagina-apache .
+sleep 10
+sudo podman run --name front-teste -d -p 8080:80 front-teste
